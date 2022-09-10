9b84dc3a816e5b0bb3f9814ddf061281c594ad54
Daniel Doyle <tr33m4n@googlemail.com>
2022-09-10 10:47:11 +0100

    Fix line length issue

--- src/CiEnvironment/CiType/AzurePipelines.php
+++ src/CiEnvironment/CiType/AzurePipelines.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace tr33m4n\CodeceptionModulePercyEnvironment\CiEnvironment\CiType;
 
-use tr33m4n\CodeceptionModulePercyEnvironment\CiEnvironment\CiType;
 use OndraM\CiDetector\Ci\AzurePipelines as CiDetectorAzurePipelines;
+use tr33m4n\CodeceptionModulePercyEnvironment\CiEnvironment\CiType;
 
 class AzurePipelines extends CiDetectorAzurePipelines implements CiTypeInterface
 {
@@ -14,8 +14,10 @@ class AzurePipelines extends CiDetectorAzurePipelines implements CiTypeInterface
      */
     public function getPullRequest(): ?string
     {
-        return ($this->env->get('SYSTEM_PULLREQUEST_PULLREQUESTID') ?: $this->env->get('SYSTEM_PULLREQUEST_PULLREQUESTNUMBER'))
-                ?: null;
+        return (
+            $this->env->get('SYSTEM_PULLREQUEST_PULLREQUESTID')
+                ?: $this->env->get('SYSTEM_PULLREQUEST_PULLREQUESTNUMBER')
+        ) ?: null;
     }
 
     /**
